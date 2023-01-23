5f1fcb07f5aa79fa125449b88ea0d3b24c5e6244
Sam Bradbury <sam.bradbury@hotmail.com>
2023-01-23 22:18:56 +1100

    added basic digital style clock layout and flashing colon

--- world-clock-app/main.js
+++ world-clock-app/main.js
@@ -1,23 +1,11 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const clockColonFlash = document.querySelector("[data-clock-flash]");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+setInterval(clockFlash, 500);
 
-setupCounter(document.querySelector('#counter'))
+function clockFlash() {
+  if (clockColonFlash.classList.contains("hide")) {
+    clockColonFlash.classList.remove("hide");
+    return;
+  }
+  clockColonFlash.classList.add("hide");
+}
