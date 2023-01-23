f743b281b1bd0fa4918cdf663087eeca1e651d65
Sam Bradbury <sam.bradbury@hotmail.com>
2023-01-23 22:38:01 +1100

    added local time functionality

--- world-clock-app/main.js
+++ world-clock-app/main.js
@@ -1,6 +1,8 @@
 const clockColonFlash = document.querySelector("[data-clock-flash]");
-
+const digits = document.getElementsByClassName("digit-number");
+updateClock();
 setInterval(clockFlash, 500);
+setInterval(updateClock, 1000);
 
 function clockFlash() {
   if (clockColonFlash.classList.contains("hide")) {
@@ -8,4 +10,19 @@ function clockFlash() {
     return;
   }
   clockColonFlash.classList.add("hide");
+}
+
+function updateClock() {
+  const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  console.log(minute);
+  const hourTenDigit = hour > 19 ? "2" : hour > 9 ? "1" : "0";
+  const hourDigit = hour > 9 ? hour.toString()[1] : hour.toString()[0];
+  const minuteTenDigit = minute.toString()[0];
+  const minuteDigit = minute.toString()[1];
+  digits[0].innerHTML = hourTenDigit;
+  digits[1].innerHTML = hourDigit;
+  digits[2].innerHTML = minuteTenDigit;
+  digits[3].innerHTML = minuteDigit;
 }
