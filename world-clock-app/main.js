2f83229856a1a9c8965a94f3e1a8f19edb52e976
Sam Bradbury <sam.bradbury@hotmail.com>
2023-01-24 22:02:50 +1100

    added timezone support and fixed digit display

--- world-clock-app/main.js
+++ world-clock-app/main.js
@@ -1,5 +1,6 @@
 const clockColonFlash = document.querySelector("[data-clock-flash]");
 const digits = document.getElementsByClassName("digit-number");
+
 updateClock();
 setInterval(clockFlash, 500);
 setInterval(updateClock, 1000);
@@ -13,14 +14,16 @@ function clockFlash() {
 }
 
 function updateClock() {
-  const date = new Date();
+  let chicago_datetime_str = new Date().toLocaleString("en-US", { timeZone: "Australia/Sydney" });
+  const date = new Date(chicago_datetime_str);
+
   const hour = date.getHours();
   const minute = date.getMinutes();
 
   const hourTenDigit = hour > 19 ? "2" : hour > 9 ? "1" : "0";
   const hourDigit = hour > 9 ? hour.toString()[1] : hour.toString()[0];
-  const minuteTenDigit = minute.toString()[0];
-  const minuteDigit = minute.toString()[1];
+  const minuteTenDigit = minute > 9 ? minute.toString()[0] : "0";
+  const minuteDigit = minute > 9 ? minute.toString()[1] : minute.toString()[0];
   digits[0].innerHTML = hourTenDigit;
   digits[1].innerHTML = hourDigit;
   digits[2].innerHTML = minuteTenDigit;
